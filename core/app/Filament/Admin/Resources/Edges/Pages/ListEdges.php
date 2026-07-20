a474190d419e08c0c13e8e7faa69bf7ed3af30bc
vaheeD khoshnouD <khoshnud@gmail.com>
2026-07-20 14:48:56 +0330

    Restore global edge reconciliation UI

--- core/app/Filament/Admin/Resources/Edges/Pages/ListEdges.php
+++ core/app/Filament/Admin/Resources/Edges/Pages/ListEdges.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Admin\Resources\Edges\Pages;
 
 use App\Filament\Admin\Resources\Edges\EdgeResource;
+use App\Http\Controllers\Admin\EdgeOperationsController;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 
 class ListEdges extends ListRecords
@@ -12,6 +15,19 @@ class ListEdges extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [CreateAction::make()];
+        return [
+            Action::make('reconcileAllDomains')
+                ->label('Reconcile all domains')
+                ->icon('heroicon-o-arrow-path')
+                ->color('warning')
+                ->requiresConfirmation()
+                ->action(function (): void {
+                    $response = app(EdgeOperationsController::class)->reconcile(request());
+                    $operation = $response->getData(true)['data'];
+                    Notification::make()->info()->title('Global edge reconciliation queued')
+                        ->body("Operation {$operation['operation_id']} will coalesce domain deployments in bounded chunks.")->send();
+                }),
+            CreateAction::make(),
+        ];
     }
 }
